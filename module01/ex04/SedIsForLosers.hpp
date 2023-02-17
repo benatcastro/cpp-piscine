8fefb5179aafc9a0fb331b91a2ed2eba77ca4737
bena <benatcastro@gmail.com>
2023-02-17 15:17:24 +0100

    reading file logic complete

---
+++ module01/ex04/SedIsForLosers.hpp
@@ -0,0 +1,15 @@
+#ifndef SED_IS_FOR_LOSERS_HPP
+# define SED_IS_FOR_LOSERS_HPP
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <sstream>
+
+# define HELPER_MSG "Sed is for losers accepts 3 arguments: <filename> <s1> <s2>"
+# define ERR_OPEN_MSG "Cant Open file: "
+# define MIN_ARGS	4
+# define FILE_INDEX 1
+# define S1_INDEX   2
+# define S2_INDEX  	3
+
+#endif
