f339824e14a4f5f77933415e87e5134aca8067ac
bena <benatcastro@gmail.com>
2023-02-19 15:16:55 +0100

    Logic complete, todo write fnc

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,33 +1,47 @@
 #include "SedIsForLosers.hpp"
 
-// static std::string *findOcurrences(const std::string s1, const std::string s2) {
-// 	return (NULL);
-// }
 
-// static std::string replaceString(const std::string s1, const std::string s2) {
-// 	std::string	outStr;
+/*
+Replace every occurrence of s1 with s2 in the string s
+*/
+static void replaceString(std::string haystack, const std::string needle, const std::string replace) {
 
-// 	return (outStr);
-// }
+	std::size_t occurrenceIndex = 0;
+	while (occurrenceIndex != haystack.npos)
+	{
+		occurrenceIndex = haystack.find(needle, occurrenceIndex);
+		haystack.erase(occurrenceIndex, needle.length());
+		haystack.insert(occurrenceIndex, replace);
+		occurrenceIndex = haystack.find(needle, occurrenceIndex);
+	}
+}
 
 int main(int argc, char **argv) {
 	std::ifstream	inFile;
 
-	if (argc != MIN_ARGS) {
+	// Init
+
+	if (argc != MIN_ARGS)
+	{
 		std::cout << HELPER_MSG << std::endl;
 		return 0;
 	}
 	inFile.open(argv[FILE_INDEX]);
-	if (!inFile.is_open()) {
+	if (!inFile.is_open())
+	{
 		std::cerr << ERR_OPEN_MSG << argv[FILE_INDEX] << std::endl;
 		return (0);
 	}
 
 	std::stringstream	inFileBuffer;
+
 	const std::string	s1(argv[S1_INDEX]);
 	const std::string	s2(argv[S2_INDEX]);
-
 	inFileBuffer << inFile.rdbuf();
-	std::cout << inFileBuffer.str();
 	inFile.close();
+
+	// Replace logic
+	replaceString(inFileBuffer.str(), s1, s2);
+
+
 }
