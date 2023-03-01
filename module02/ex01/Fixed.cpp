40e0106db591ab7236b0f337d09b47334fce0d61
Benat Castro <becastro@c4r7s3.42urduliz.com>
2023-03-01 06:24:41 +0100

    finished

---
+++ module02/ex01/Fixed.cpp
@@ -0,0 +1,39 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+    std::cout << "Default constructor\n";
+    this->_fixedValue = 0;
+}
+
+Fixed::Fixed(const int n): _fixedValue(n << 8) {
+    std::cout << "Int constructor\n";
+}
+
+Fixed::Fixed(const float f): _fixedValue(roundf(f * (1 << 8))) {
+    std::cout << "Float constructor\n";
+}
+Fixed::Fixed(const Fixed &obj) {
+    std::cout << "Copy constructor called\n";
+    this->_fixedValue = obj._fixedValue;
+}
+
+int Fixed::getFixedValue() const {
+    return (this->_fixedValue);
+}
+
+std::ostream  &operator<<(std::ostream &os, const Fixed &obj) {
+    os << obj.toFloat();
+    return (os);
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called\n";
+}
+
+int Fixed::toInt(void) const {
+    return (this->_fixedValue >> this->_bits);
+}
+
+float Fixed::toFloat(void) const {
+    return ((float)this->_fixedValue / 256);
+}
---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <cmath>
+# include <iostream>
+
+class Fixed {
+    public:
+        Fixed(void);                        // Default constructor
+        Fixed(const int n);                 // Integer overload
+        Fixed(const float f);               // Float overload
+        Fixed(const Fixed &obj);            // Copy constructor
+        ~Fixed();                           // Destructor
+//        Fixed   &operator=(Fixed &obj);      // Assignment operator
+        int     getFixedValue(void) const;
+        float   toFloat(void) const;
+        int     toInt(void) const;
+    private:
+        int                 _fixedValue;
+        static const int    _bits = 8;
+};
+
+std::ostream  &operator<<(std::ostream &os, const Fixed &obj);
+
+#endif
