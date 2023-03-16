be5dd37db3b985b343e7b8a9259b44b8605993a6
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-03-16 19:32:05 +0100

    header declarations and files

---
+++ module03/ex00/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+#define ENERGY_COST 1
+
+class ClapTrap {
+public:
+    ClapTrap(); // Default constructor
+    ClapTrap(ClapTrap &obj); // Copy constructor
+    ~ClapTrap();
+    ClapTrap    &operator=(ClapTrap const &obj); // Assignment overload
+    void        attack(const std::string &target);
+    void        takeDamage(unsigned int amount);
+    void        beRepaired(unsigned int amount);
+private:
+    std::string _name;
+    u_int32_t _ep;
+    u_int32_t _hp;
+    u_int32_t _ad;
+};
+
+#endif
