87afc8cbbbbe42dcf774c3a4bd4e3f4776a59f78
becastro <benatcastro@gmail.com>
2023-05-22 02:44:54 +0200

    iter finished

--- module07/ex01/Iter.hpp
+++ module07/ex01/Iter.hpp
@@ -1,13 +1,18 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+
+
 
 template <typename T>
-void	iter(T *array, T len, T f(T))
+void	iter(T *array, std::size_t len, void (*f)(T &))
 {
-	for (T i; i < len; i++)
+	if (!f || !array)
+		return;
+	for (std::size_t i = 0; i < len; i++)
 	{
-		(f)array[i];
+		(f)(array[i]);
 	}
 }
 
---
+++ module07/ex01/main.cpp
@@ -0,0 +1,43 @@
+#include "Iter.hpp"
+#include <string.h>
+
+
+class Awesome {
+public:
+	Awesome(void): _n(42) {return;};
+	int get(void) const {return this->_n; }
+private:
+	int _n;
+};
+
+std::ostream & operator<<(std::ostream &o, Awesome const &rhs) {o << rhs.get(); return o;}
+template <typename T>
+void print(T const &x)
+{
+	std::cout << x << std::endl;
+	return;
+}
+
+
+int main(void)
+{
+	std::cout << "Array of integers\n";
+	const int int_tab[] = {1, 2, 3, 4};
+
+	iter(int_tab, 4, &print);
+
+	std::cout << "Array of doubles\n";
+	const double double_tab[] = {1.2, 2.3, 3.4, 4.5};
+
+	iter(double_tab, 4, print);
+
+	std::cout << "Array of strings\n";
+	const std::string str_tab[] = {"hola", "adios", "str1", "str2"};
+
+	iter(str_tab, 4, print);
+
+	std::cout << "Array of Awesome\n";
+	const Awesome awesome_tab[5];
+
+	iter(awesome_tab, 5, print);
+}
