96df0918af7ba87305f1fa38e69ec3b1b586ffc0
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-05-23 14:37:25 +0200

    done

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -15,14 +15,41 @@ public:
 	MutantStack() { cout << "MutantStack constructed\n"; };
 	~MutantStack() { cout << "MutantStack destructed\n"; };
 
+	MutantStack(MutantStack<T> &obj) {
+		cout << "MutantStack Constructed by copy\n";
+		this->c = obj.c;
+	};
+
+	MutantStack &operator=(MutantStack<T> &obj) {
+		cout << "MutantStack operator=\n";
+		this->c = obj.c;
+		return *this;
+	};
+
 	typedef typename MutantStack<T>::container_type::iterator iterator;
+	typedef typename MutantStack<T>::container_type::reverse_iterator reverse_iterator;
+
+
+
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
 
 	iterator begin() { return this->c.begin(); }
-
 	iterator end() { return this->c.end(); }
-
-
-
 };
+
+template <typename T>
+typename MutantStack<T>::iterator& operator++(typename MutantStack<T>::iterator& it) {
+	cout << "test\n";
+	++it;
+	return it;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, MutantStack<T> &rhs) {
+	for (typename MutantStack<T>::iterator it = rhs.begin(); it != rhs.end(); it++)
+		os << *it << " ";
+	return os;
+}
 
 #endif
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,28 +1,111 @@
 #include "MutantStack.hpp"
-
+#include <vector>
+#include "list"
 
 int main(void)
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << "ms top: " << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
+	cout << "\n\n****Mutant Stack basic functions****\n\n";
+	try {
+		std::stack<int> stack;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		stack.push(5);
+		mstack.push(17);
+		stack.push(17);
+		mstack.push(3);
+		stack.push(3);
+		mstack.push(5);
+		stack.push(5);
+		mstack.push(737);
+		stack.push(737);
+		cout << "Ms-> " << mstack << endl;
+		mstack.pop();
+		stack.pop();
+		cout << "Ms after pop-> " << mstack << endl;
+		cout << "Ms top-> " << mstack.top() << " | stack top->" << stack.top() << endl;
+		cout << "Ms size-> " << mstack.size() << " | stack size->" << stack.size() << endl;
 	}
-	std::stack<int> s(mstack);
+	catch (std::exception &e) {
+		cout << e.what() << endl;
+	}
+
+	MutantStack<int> original;
+	original.push(1);
+	original.push(2);
+	original.push(3);
+	cout << "Original: " << original << endl;
+	{
+		MutantStack<int> copy(original);
+		cout << "Copy: " << copy << endl;
+	}
+	cout << "Original after cons copy: " << original << endl;
+
+	cout << "Original: " << original << endl;
+	{
+		MutantStack<int> assignment;
+		assignment = original;
+		cout << "Copy: " << assignment << endl;
+	}
+	cout << "Original after cons copy: " << original << endl;
+
+	cout << "\n\n****Mutant Stack iterators functions****\n\n";
+	try {
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+
+		for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+			cout << *it << " ";
+		cout << endl;
+		for (MutantStack<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++)
+			cout << *it << " ";
+		cout << endl;
+
+	}
+	catch (std::exception &e) {
+		cout << e.what() << endl;
+	}
+
+
+	cout << "\n\n****List comparation****\n\n";
+
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	try {
+		for (std::list<int>::iterator it = list.begin(); it != list.end(); it++)
+			cout << *it << " ";
+		cout << endl;
+		for (std::list<int>::reverse_iterator it = list.rbegin(); it != list.rend(); it++)
+			cout << *it << " ";
+		cout << endl;
+
+	}
+	catch (std::exception &e) {
+		cout << e.what() << endl;
+	}
+
+	cout << "\n\n****Construction****\n\n";
+
+	cout << "Original: " << original << endl;
+	{
+		MutantStack<int> copy(original);
+		cout << "Copy: " << copy << endl;
+	}
+	cout << "Original after cons copy: " << original << endl;
+	{
+		MutantStack<int> assignment;
+		assignment = original;
+		cout << "Assignment: " << assignment << endl;
+	}
+	cout << "Original assignment: " << original << endl;
+
+
 	return 0;
 }
